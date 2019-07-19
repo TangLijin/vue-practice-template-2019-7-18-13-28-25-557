a01768be5194de679138d3932074c3a82118344e
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-19 15:22:25 +0800

    feat:first task,new VUE projuct

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <div id="app">
+  <!--
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p> {{count}}</p>
+    <button v-on:click ="add">点我+</button>
+    <button v-on:click ="decrease">点我-</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { METHODS } from 'http';
+//import HelloWorld from './components/HelloWorld.vue'
 
+/*
 export default {
   name: 'app',
   components: {
     HelloWorld
   }
-}
+} */
+
+
+export default {
+  name: 'app',
+  data: function(){
+      return {
+        count: 0
+      } 
+  },
+  methods: {
+    add:function() {
+      this.count++;
+    },
+    decrease:function(){
+      this.count--;
+    }
+  }
+};
+
 </script>
+
 
 <style>
 #app {
